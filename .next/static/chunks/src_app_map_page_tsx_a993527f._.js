0f4860a174d984b1446a263a54345470b36b8e83
Jedremi <69273603+Jedremi@users.noreply.github.com>
2025-06-27 10:57:25 -0300

    Add .next/static/chunks/src_app_map_page_tsx_a993527f._.js 1751032644001

---
+++ .next/static/chunks/src_app_map_page_tsx_a993527f._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_mapbox-gl_dist_mapbox-gl_0f9823ba.js",
+  "static/chunks/node_modules_9776b4ab._.js",
+  "static/chunks/src_5006ce6b._.js",
+  "static/chunks/node_modules_mapbox-gl_dist_mapbox-gl_0eb0dc9d.css"
+],
+    source: "dynamic"
+});
